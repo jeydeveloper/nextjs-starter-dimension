077c62759aa2b250aa93b04de7f05dbb9e4770d1
Jey Developer <jey.developer@google.com>
2020-08-01 13:49:10 +0700

    update load font

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ class IndexPage extends React.Component {
                 <div>
                     <Head>
                         <title>Next.js Starter</title>
-                        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i" rel="stylesheet" />
+                        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i&display=swap" rel="stylesheet" />
                     </Head>
 
                     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
